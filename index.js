56985b71f6bba24f31ebc739774bdf2f3c421c71
Shubinder <shubinder92@gmail.com>
2019-07-24 09:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday) {
   return "Happy ${holiday}, you!"
 }
+
+function holidayCountdown('holiday', 'days') {
+  return "It\'s ${days} days until ${holiday}!"
+}
